d0bd1612d5f9ab7d4df4434406c54b09ad4a212c
Kati Winkler <katimcfarland1@gmail.com>
2021-02-08 16:18:46 -0500

    Codewar challenge

---
+++ secure.js
@@ -0,0 +1,8 @@
+const alphanumeric = (str) => {
+    if (!str.length) return false;
+    const ALLOWED_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    for (let char of str) {
+      if (!ALLOWED_CHARS.includes(char)) return false;
+    }
+    return true;
+  };
